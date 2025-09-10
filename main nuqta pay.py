379ce82c5c07cb12f20cb5690b72027269b624b6
Qalandarov Ibrohimjon <qalandarovi45@gmail.com>
2025-09-10 18:34:18 +0500

    Add files via upload

---
+++ main nuqta pay.py
@@ -0,0 +1,84 @@
+# #№1
+# P=float(input())
+# k=1.05
+# t=0
+# aim=P*2
+# while P<aim:
+#     P=P*k
+#     t+=1
+# print(t)
+
+# #№2
+# B=1
+# t=0
+# aim=1_000_000
+# while B<aim:
+#     B*=2
+#     t+=1
+# print(t)
+
+# #№3
+# q=50_000
+# k=0.05
+# p=int(input())
+# t=0
+# while p<q:
+#     q+=q*k
+#     q-=p
+#     t+=1
+# print(t)
+
+# #№4
+# b=100
+# k=15
+# t=0
+# while b>20:
+#     b-=k
+#     t+=1
+# print(t)
+
+# #№5
+# O=int(input())
+# s=int(input())
+# k=int(input())
+# t=0
+# if k>s:
+#     print('Hechchon')
+# else:
+#     while O>0:
+#         O-=s
+#         O+=k
+#         t+=1
+#     print(t)
+
+# #№6
+# I=0
+# ch=50
+# k=200
+# t=0
+# while I < 2000:
+#     I+=200
+#     I-=50
+#     t+=1
+# print(t)
+
+# #№7
+# B=60
+# y=8
+# m=0
+# kpl=100/8
+# while B>0:
+#     B-=1
+#     m+=kpl
+# print(m)
+
+# #№8
+# O=100000
+# k=1.03
+# aim=200_000
+# t=0
+# while O<aim:
+#     O*=k
+#     t+=1
+# print(t)
+
